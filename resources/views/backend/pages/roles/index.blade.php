83ed04f1a089d82dee8a1f98888a281d268f4d7d
asraf-mridha <asrafmridha@gmail.com>
2023-01-17 09:44:57 +0600

    Design Checkbox for permission

--- resources/views/backend/pages/roles/index.blade.php
+++ resources/views/backend/pages/roles/index.blade.php
@@ -16,65 +16,47 @@
 
 @section('content')
 
-<div class="row" id="white-table" style="margin-left: 300px">
-    <div class="col-12">
-        <div class="card">
-            @if(Session::has('success'))
-                <p class="alert alert-info">{{ Session::get('success') }}</p>
-            @endif
-            <div class="card-header">
-                <h4 class="card-title">Total Role ({{ count($role) }})</h4>
-                <h6 class="card-title text-right"><button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Create Role</button> </h6>
-            </div> 
-           
-            <div class="table-responsive " >
-                <table class="table table-white " >
-                    <thead>
-                            
-                        <tr>
-                            <th>sl</th>
-                            <th> Name</th>
-                            <th> Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($role as $role)
-                        <tr>
-                            <td>{{ $loop->index+1 }}</td>
-                            <td> {{ $role->name }}</td>
-                            <td> <button class="btn btn-primary">Edit</button>
-                            <button class="btn btn-danger">Delete</button>
-                            
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+    <div class="row" id="white-table" style="margin-left: 300px">
+        <div class="col-12">
+            <div class="card">
+                @if(Session::has('success'))
+                    <p class="alert alert-info">{{ Session::get('success') }}</p>
+                @endif
+                <div class="card-header">
+                    <h4 class="card-title">Total Role ({{ count($role) }})</h4>
+                    <h6 class="card-title text-right"><a class="btn btn-primary" href="{{ route('roles.create') }}">Create Role</a> </h6>
+                </div> 
+            
+                <div class="table-responsive " >
+                    <table class="table table-white " >
+                        <thead>
+                                
+                            <tr>
+                                <th>sl</th>
+                                <th> Name</th>
+                                <th> Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($role as $role)
+                            <tr>
+                                <td>{{ $loop->index+1 }}</td>
+                                <td> {{ $role->name }}</td>
+                                <td> <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                                
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-
-            <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <form action="{{ route('roles.store') }}" method="POST">
-        @csrf
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Create Role</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <input type="text" name="name" class="form-control" placeholder="Role">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save</button>
-      </div>
+        </div>
     </div>
-    </form>
-  </div>
-</div>
+
+
+
 
 
 @endsection
