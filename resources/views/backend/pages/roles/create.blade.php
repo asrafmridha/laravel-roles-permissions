ad9187cd81da8fe16700a8bf9736746635b0fae1
asraf-mridha <asrafmridha@gmail.com>
2023-01-19 11:09:01 +0600

    GroupWise Permisiion done

--- resources/views/backend/pages/roles/create.blade.php
+++ resources/views/backend/pages/roles/create.blade.php
@@ -28,26 +28,64 @@
                             <h5 class="modal-title" id="exampleModalLabel">Create Role</h5>
                         </div>
                         @include('backend.partial.message')
-                    <div class="modal-body">
-                   
-                        <input type="text" name="name" class="form-control" placeholder="Role">
-                          <br><br>
-                          <h5>Permissions</h5>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="checkPermissionAll" checked>
-                            <label class="form-check-label" for="flexCheckChecked">
-                                All
-                            </label>
+                        <div class="modal-body">
+
+                            <input type="text" name="name" class="form-control" placeholder="Role">
+                            <br><br>
+                            <h5>Permissions</h5>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="checkPermissionAll" checked>
+                                <label class="form-check-label" for="flexCheckChecked">
+                                    All
+                                </label>
+                            </div>
+                                @php
+                                    $i=1;
+                                @endphp
+
+                                @foreach ($PermissionGroup as $group)
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="{{ $i }}Management" value="{{ $group->name }}" onclick="checkPermissionByGroup('role-{{ $i }}-management-checkbox', this)">
+                                                <label class="form-check-label" for="checkPermission">{{ $group->name }}</label>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-9 role-{{ $i }}-management-checkbox">
+                                            @php
+                                              $permission=DB::table('permissions')->where('group_name',$group->name)->get();
+                                              
+                                            // $permission = App\Models\User::getPermissionGroup($group->name);
+                                            //  $j = 1;
+                                            @endphp
+                                            @foreach ($permission as $permission)
+
+                                            {{-- @php
+                                                dd($permission);
+                                            @endphp --}}
+                                                <div class="form-check mt-3">
+                                                    <label class="ckbox">
+                                                        <input type="checkbox" id="permissionCheck{{ $permission->name }}" value=" {{ $permission->id }}" name="permissions[]">
+                                                        <span>{{ $permission->name }}</span>
+                                                    </label>
+                                                </div> 
+                                                {{-- @php  $j++; @endphp --}}
+                                            @endforeach 
+                                        </div>
+                                    </div>
+                                       {{-- @php  $i++; @endphp  --}}
+                                @endforeach
+
+                                {{-- @foreach ($permission as $permission)
+                                <div class="form-check mt-3">
+                                    <label class="ckbox">
+                                        <input type="checkbox" id="permissionCheck{{ $permission->id }}" value=" {{ $permission->id }}" name="permissions[]">
+                                        <span>{{ $permission->name }}</span>
+                                    </label>
+                                </div>
+                                @endforeach     --}}
                         </div>
-                    @foreach ($permission as $permission)
-                        <div class="form-check mt-3">
-                            <label class="ckbox">
-                                <input type="checkbox" id="permissionCheck{{ $permission->id }}" value=" {{ $permission->id }}" name="permissions[]">
-                                <span>{{ $permission->name }}</span>
-                            </label>
-                        </div>
-                    @endforeach    
-                    </div>
                     </div>
                   
                     <div class="modal-footer">
@@ -75,5 +113,3 @@
         });
     </script>
 @endsection    
-
-
